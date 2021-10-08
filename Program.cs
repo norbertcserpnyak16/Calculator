ddc3973d0e336633a153d73ad6018a75a7670ca1
Norbert Cserpnyák <50951721+norbertcserpnyak16@users.noreply.github.com>
2021-10-08 21:12:28 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+
+namespace Szamologep
+{
+	class Program
+	{
+
+		static void Main(string[] args)
+		{
+
+			//változókkal való műveletek!
+			Console.WriteLine("Enter a number!");
+			double a = double.Parse(Console.ReadLine());
+			Console.WriteLine("Enter another number!");
+			double b = double.Parse(Console.ReadLine());
+			double ab = Math.Round(a + b, 4);
+			double ab2 = Math.Round(a - b, 4);
+			double ab3 = Math.Round(a * b, 4);
+			double ab4 = Math.Round(a / b, 4);
+
+			//Változókkal való müveletek eredményének a kiiratása!
+			Console.WriteLine($"This is the result of adding variables A and B: {ab}");
+			Console.WriteLine($"This is the result of subtracting variables A and B: {ab2}");
+			Console.WriteLine($"This is the result of multiplying variables A and B: {ab3}");
+			Console.WriteLine($"Result of dividing variables A and B: {ab4}");
+		}
+
+		static bool Paros(int szam)
+		{
+			return szam % 2 == 0;
+		}
+		static bool Pozitiv(double number)
+		{
+			return number >= 0;
+		}
+	}
+}
+
+
